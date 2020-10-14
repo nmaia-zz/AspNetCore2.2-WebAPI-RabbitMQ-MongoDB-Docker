509344f10390cc78f1846e59d645f5656a445979
Natanael <gm.natanael@gmail.com>
2020-10-14 19:03:34 -0300

    Implementando mecanica de escrita de mensagem no rabbitmq. Ajustes no arquivo docker-compose.

--- Demo/src/Demo.API/Controllers/PesquisasController.cs
+++ Demo/src/Demo.API/Controllers/PesquisasController.cs
@@ -1,6 +1,8 @@
 ﻿using Demo.API.Context;
 using Demo.API.Models;
+using Demo.API.RabbitMQ;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 
 namespace Demo.API.Controllers
@@ -20,6 +22,9 @@ namespace Demo.API.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Pesquisa>> Get()
         {
+            var rabbitmq = new Mensagem();
+            rabbitmq.Publicar();
+
             return Ok(_mongoContext.ObterTodos<Pesquisa>());
         }
 
---
+++ Demo/src/Demo.API/RabbitMQ/Mensagem.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Demo.API.RabbitMQ
+{
+    public class Mensagem
+    {
+        ConnectionFactory connectionFactory = new ConnectionFactory()
+        {
+            Uri = new Uri(@"amqp://user:pass@rabbitmq:5672/challenge-dev"),
+            NetworkRecoveryInterval = TimeSpan.FromSeconds(10),
+            AutomaticRecoveryEnabled = true
+        };
+
+        public void Publicar()
+        {
+            using (var connection = connectionFactory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "myqueue",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                    );
+
+                    string message = "Minha Mensagem - " + DateTime.Now.ToString();
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "myqueue",
+                        basicProperties: null,
+                        body: body
+                    );
+                }
+            }
+        }
+
+        public void Consomir()
+        {
+
+        }
+    }
+}
